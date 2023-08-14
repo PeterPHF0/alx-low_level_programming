85125d226b9d2002a1c007a58c74151c6493138a
peter <peter.hani03@gmail.com>
2023-08-14 14:42:54 -0700

    new

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - kxopam
+ *
+ * Description: hhh
+ *
+ * Return: cskdvj
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
