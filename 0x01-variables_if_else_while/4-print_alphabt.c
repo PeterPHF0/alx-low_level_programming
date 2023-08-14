dd01dd6b084cab98044b8ff2dd3267648ef0f239
peter <peter.hani03@gmail.com>
2023-08-14 14:37:31 -0700

    new

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - kxopam
+ *
+ * Description: hhh
+ *
+ * Return: cskdvj
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'q' || a != 'e')
+		{
+			printf("%c", a);
+			a++;
+		}
+	}
+	printf("\n");
+	return (0);
+}
