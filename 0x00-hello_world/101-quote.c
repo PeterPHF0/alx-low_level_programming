0d73199e01693b48a47e3efe35e471490f7190a8
peter <peter.hani03@gmail.com>
2023-08-14 08:12:33 -0700

    new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 */
 int main(void)
 {
-	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, quote, 58);
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quote, 59);
 	return (1);
 }
