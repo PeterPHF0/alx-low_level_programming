cd1744a6436518c58a3ca31f3b74406801bccc36
peter <peter.hani03@gmail.com>
2023-08-16 14:19:40 -0700

    new

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+void jack_bauer(void)
+{
+	int i = 0, j = 0, c = 0, v = 0;
+
+	while (i <= 2)
+	{
+		while (j <= 3)
+		{
+			while (c <= 5)
+			{
+				while(v <= 9)
+				{
+					_putchar((char)i);
+					_putchar((char)j);
+					_putchar(':');
+					_putchar((char)c);
+					_putchar((char)v);
+					v++;
+				}
+				c++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
