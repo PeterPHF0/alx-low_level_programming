4ffe7bc542b1c8ad6c9e718b80d96244069edcdd
peter <peter.hani03@gmail.com>
2023-08-16 12:48:20 -0700

    new

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * _isalpha - checks for a given input is a letter or not 
+ *
+ * @c: char as an input
+ *
+ * Return: 0 or 1
+*/
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
