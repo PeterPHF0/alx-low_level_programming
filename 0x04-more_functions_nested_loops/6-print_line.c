3145af64eb1d5f592761f1129e8f544531938268
peter <peter.hani03@gmail.com>
2023-08-18 13:54:56 -0700

    new

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_line - Entry point
+ *
+ * @n: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
