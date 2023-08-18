ff53a3b678790f54809e773e1057511a03eccdd9
peter <peter.hani03@gmail.com>
2023-08-18 13:18:44 -0700

    new

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * print_most_numbers - Entry point
+ *
+ * Description: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+
+void print_most_numbers(void)
+{
+	int zero = 48;
+
+	while (zero <= 57)
+	{
+		if (zero != 2 && zero != 4)
+		{
+			_putchar(zero);
+		}
+	}
+	_putchar('\n');
+}
