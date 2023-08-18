be514c711629bbf706610a2b5228665e8819e7a6
peter <peter.hani03@gmail.com>
2023-08-18 12:56:51 -0700

    new

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ *  _isdigit - Entry point
+ *
+ * @c: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+int _isdigit(int c)
+{
+	int zero = 0;
+
+	while (zero <= 9)
+	{
+		if (c == zero)
+		{
+			return (1);
+		}
+		zero++;
+	}
+	return (0);
+}
