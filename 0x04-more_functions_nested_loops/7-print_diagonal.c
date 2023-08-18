7925b717967a90d368cc1bfb01adc329a0254df2
peter <peter.hani03@gmail.com>
2023-08-18 14:32:44 -0700

    new

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_diagonal - Entry point
+ *
+ * @n: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+void print_diagonal(int n)
+{
+	int i = 0, j = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < n)
+		{
+			while (j < n)
+			{
+				for (int i = 0; i < j; i++)
+				{
+					_putchar('\');
+				}
+				j++;
+				_putchar('\n');
+			}
+		}
+	}
+
+}
