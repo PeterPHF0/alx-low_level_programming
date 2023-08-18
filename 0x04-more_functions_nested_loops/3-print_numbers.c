932b306ffebc167486e9085222d6612a1626a0c8
peter <peter.hani03@gmail.com>
2023-08-18 13:09:32 -0700

    new

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include" main.h"
+/**
+ * print_numbers - Entry point
+ *
+ * Description: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+}
