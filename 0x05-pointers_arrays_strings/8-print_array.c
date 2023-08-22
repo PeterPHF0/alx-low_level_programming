c670a270612cafe50355c206efe210f51aa3fbbd
peter <peter.hani03@gmail.com>
2023-08-22 09:26:55 -0700

    new

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,14 +14,14 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	if (n <= 0)
+	if (!(n <= 0))
 	{
-		while (i != (n-1))
+		while (i != (n - 1))
 		{
 			printf("%d, ", a[i]);
 			i++;
 		}
-		printf("%d", a[n-1]);
+		printf("%d", a[n - 1]);
 	}
 	printf("\n");
 }
