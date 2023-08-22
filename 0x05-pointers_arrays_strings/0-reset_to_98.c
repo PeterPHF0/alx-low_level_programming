77be5c5b0a42f5fe9875e05a19219388a9402508
peter <peter.hani03@gmail.com>
2023-08-22 03:06:33 -0700

    new

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * reset_to_98 - change the value in the address that the pointer holds
+ *
+ *@n: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
