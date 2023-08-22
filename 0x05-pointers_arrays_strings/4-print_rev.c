a1ae6461c3eda56f48aebe9a5ddaffbf94ec7492
peter <peter.hani03@gmail.com>
2023-08-22 04:07:40 -0700

    new

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,13 +12,14 @@ void print_rev(char *s)
 {
 	int len = 0;
 	char *str = s;
+	int ind;
 
 	while (*str != '\0')
 	{
 		len++;
 		str++;
 	}
-	int ind = len - 1;
+	ind = len - 1;
 
 	while (ind != -1)
 	{
