70aa18cea4b644b6609f0d718e3509ffd4a828bf
peter <peter.hani03@gmail.com>
2023-08-22 03:38:04 -0700

    new

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+
+void print_rev(char *s)
+{
+	int len = _strlen(*s);
+
+	for (int i = len-1 ;i <= 0 ;i--)
+	{
+		_putchar(str[i]);
+	}
+
+}
