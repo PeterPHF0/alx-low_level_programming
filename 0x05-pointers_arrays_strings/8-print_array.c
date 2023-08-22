313934a58ec2e16673ae937c5e2fa4415b1a516f
peter <peter.hani03@gmail.com>
2023-08-22 09:16:14 -0700

    new

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_array - Entry point
+ *
+ * @a: use sizeof
+ *
+ * @n: kogkm
+ *
+ * Return: 0 (Success)
+*/
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i != (n-1))
+	{
+		printf("%d, ", a[i]);
+		i++
+	}
+	printf("%d", a[n-1]);
+}
