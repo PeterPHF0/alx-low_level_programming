4dbdce04de72f6415f6f4c27000e6a1519fb756c
peter <peter.hani03@gmail.com>
2023-08-25 15:05:17 -0700

    new

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include <string.h>
+/**
+ * _strncpy - Entry point
+ *
+ * @dest: 1st string
+ *
+ * @src: 2nd string
+ *
+ * @n: number of concatenated char
+ *
+ * Return: 0 (Success)
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int counter = 0;
+
+	while (counter < n && *src != '\0')
+	{
+		dest[counter] = *src;
+		src++;
+		counter++;
+	}
+	return (dest);
+}
