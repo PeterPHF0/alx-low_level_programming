ed71215290ef1271536118a11b196e9b3a365bcf
peter <peter.hani03@gmail.com>
2023-08-25 17:02:49 -0700

    new

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -23,9 +23,9 @@ char *_strncpy(char *dest, char *src, int n)
 		counter++;
 	}
 	while (*src == '\0' && counter < n)
-        {
-            dest[counter] = '\0';
-            counter++;
-        }
+	{
+		dest[counter] = '\0';
+		counter++;
+	}
 	return (dest);
 }
