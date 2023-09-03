14a2aee040641f6b1db8bdef8d66239f2e43b66c
peter <peter.hani03@gmail.com>
2023-09-03 03:16:11 -0700

    new

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(*(++s));
+	}
+	_putchar('\n');
+}
