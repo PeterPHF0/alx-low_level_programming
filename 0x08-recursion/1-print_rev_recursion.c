9a9266c664ea858c64a5b45bf46d7d8ba39143f8
peter <peter.hani03@gmail.com>
2023-09-04 05:17:11 -0700

    new

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _print_rev_recursion - Entry point
+ *
+ * @s: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return ;
+	_print_rev_recursion(++s);
+	_putchar(*(s-1));
+}
