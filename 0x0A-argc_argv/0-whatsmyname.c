afc51762101a1367b3bb278e8623cd48bf4c2750
peter <peter.hani03@gmail.com>
2023-09-06 14:26:29 -0700

    new

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * @argc: num of command-line arguments
+ *
+ * @argv: array of strings(pointers to char)
+ *
+ * Return: 0 (Success)
+*/
+
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
