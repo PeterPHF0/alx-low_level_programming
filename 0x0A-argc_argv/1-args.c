abba1d2939b7073c237e687f2c30f1310f47f424
peter <peter.hani03@gmail.com>
2023-09-06 14:35:57 -0700

    new

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * @argc: use sizeof
+ *
+ * @argv:mviodkf
+ *
+ * Return: 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
