1354655d84e8ea579d0d1ea051006aed9aeefcbd
peter <peter.hani03@gmail.com>
2023-09-06 18:45:58 -0700

    new

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,4 @@
-
+#include <stdlib.h>
 #include<stdio.h>
 /**
  * main - Entry point
@@ -14,11 +14,12 @@ int main(int argc, char *argv[])
 {
 	if (argc < 3)
 	{
-		printf("Error");
+		printf("Error\n");
+		return (1);
 	}
 	else
 	{
-		printf("%d\n", argv[1] * argv[2]);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
 	return (0);
 }
