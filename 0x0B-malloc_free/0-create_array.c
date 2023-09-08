7dd753dd2debb7476db54e38a4a0a691c3a64bfa
peter <peter.hani03@gmail.com>
2023-09-08 15:25:14 -0700

    new

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+
+	if (size <= 0)
+	{
+		return (NULL);
+	}
+	arr = (*char)malloc(size * sizeof(char));
+	arr[0] = c;
+	return (arr);
+}
