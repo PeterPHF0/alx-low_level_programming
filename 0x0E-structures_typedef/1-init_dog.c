733eba489fc2d0159a9ab9b75c2327d682314d95
peter <peter.hani03@gmail.com>
2023-09-12 14:28:27 -0700

    new

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * init_dog - Entry point
+ *
+ * @dog: vdfv
+ * @name:ffvf
+ * @age:vfv
+ * @owner: vdfv
+ *
+ * Description: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
