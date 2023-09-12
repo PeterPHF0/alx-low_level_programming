84d7b84cb4f3c2ec76af4f5b21f474af0e6cf92f
peter <peter.hani03@gmail.com>
2023-09-12 09:00:14 -0700

    new

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#define NULL ""
+/**
+ * main - Entry point
+ *
+ * Description: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int len = strlen(s1) + strlen(s2) + 1;
+	char *conc;
+	int i = 0,j = 0;
+	if (s1 == NULL)
+		s1 = "";
+	else if (s2 == 	NULL)
+		s2 = "";
+	conc = (char*)malloc(len * sizeof(char));
+	if (conc == NULL)
+	{
+		return (NULL);
+	}
+	while (i < strlen(s1))
+	{
+		conc[i] = s1[i];
+		i++;
+	}
+	while (j <= strlen(s2))
+	{
+		conc[i] = s1[j];
+		i++;
+		j++;
+        }
+	return (conc);
+}
