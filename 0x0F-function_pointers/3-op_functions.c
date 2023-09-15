2db84aea92695abbf00676ef0d5faaa1d630a738
peter <peter.hani03@gmail.com>
2023-09-15 13:48:34 -0700

    new

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include"3-calc.h"
+/**
+ * main - Entry point
+ *
+ * Description: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+int op_add(int a, int b)
+{
+        return (a + b);
+}
+/**
+ * main - Entry point
+ *
+ * Description: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+/**
+ * main - Entry point
+ *
+ * Description: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * main - Entry point
+ *
+ * Description: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+/**
+ * main - Entry point
+ *
+ * Description: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
