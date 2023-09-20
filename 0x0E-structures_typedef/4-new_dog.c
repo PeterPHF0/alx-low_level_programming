507ded302bd47c549db5236a6272736e256cb6da
peter <peteriskander75@gmail.com>
2023-09-20 04:10:33 -0700

    new

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t dog;
+
+	dog.name = name;
+	dog.age = age;
+	dog.owner = owner;
+	dog_name = (char *)malloc(strlen(name) * sizeof(char));
+	if (dog_name == NULL)
+		return (NULL);
+	dog_ow = (char *)malloc(strlen(name) * sizeof(char));
+	if(dog_owner == NULL)
+		return (NULL);
+	return (dog);
+}
