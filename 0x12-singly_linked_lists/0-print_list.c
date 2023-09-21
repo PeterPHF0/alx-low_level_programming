5044c1822053512b9d8630fe03d18df15b9230ae
peter <peteriskander75@gmail.com>
2023-09-21 12:37:03 -0700

    new

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include "lists.h"
+/**
+ * print_list - Entry point
+ *
+ * @h: pointer to the header node
+ *
+ * Return: 0 (Success)
+*/
+size_t print_list(const list_t *h)
+{
+	size_t len = 1;
+
+	if (h == NULL)
+		return (0);
+	while (h->next != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
