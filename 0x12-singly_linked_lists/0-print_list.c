d97709d003dde2be42c2f7b6e5f0c20ba5a9f5a3
peter <peteriskander75@gmail.com>
2023-09-21 13:07:10 -0700

    new

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,10 +13,10 @@ size_t print_list(const list_t *h)
 
 	if (h == NULL)
 		return (0);
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		len++;
