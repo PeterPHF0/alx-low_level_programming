ac886bd598f27ef900f5d05d4c40933cfd17edcc
peter <peteriskander75@gmail.com>
2023-09-22 14:08:54 -0700

    new

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,8 +7,19 @@
  * Return: 0 (Success)
 */
 
+int get_sqrt(int n, int i)
+{
+	if (i == n)
+		return (-1);
+	if (n == 1 || n == 0)
+		return (n);
+	return get_sqrt(n, 2);
+}
 int _sqrt_recursion(int n)
 {
-	
-
+	if (n < 0)
+		return (-1);
+	if (n == 1 || n ==0 )
+		return n;
+	return get_sqrt(n, 2);
 }
