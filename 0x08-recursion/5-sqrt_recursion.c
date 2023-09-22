a50cc55eeeeb277bc5a97ac20cdd126a36ec30de
peter <peteriskander75@gmail.com>
2023-09-22 14:12:47 -0700

    new

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,32 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * _sqrt_recursion - Entry point
  *
- * Description: use sizeof
+ * @n: use sizeof
  *
  * Return: 0 (Success)
 */
-
+/**
+ * get_sqrt - Entry point
+ *
+ * @n: use sizeof
+ * @i: cc
+ *
+ * Return: 0 (Success)
+*/
 int get_sqrt(int n, int i)
 {
 	if (i == n)
 		return (-1);
-	if (n == 1 || n == 0)
-		return (n);
-	return get_sqrt(n, 2);
+	if (i * i == n)
+		return (i);
+	return get_sqrt(n, i + 1);
 }
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 1 || n ==0 )
+	if (n == 1 || n == 0 )
 		return n;
 	return get_sqrt(n, 2);
 }
