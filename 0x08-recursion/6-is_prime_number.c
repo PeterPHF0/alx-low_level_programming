ece1362f2f2929bb84510dc6534766e344aeaedb
peter <peteriskander75@gmail.com>
2023-09-22 23:35:33 -0700

    new

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,19 +1,6 @@
 #include<stdio.h>
 #include "main.h"
 /**
- * is_prime_number - Entry point
- *
- * @n: use sizeof
- *
- * Return: 0 (Success)
-*/
-int is_prime_number(int n)
-{
-	if (n == 1)
-		return (1);
-	return (is_divisible_by_i(n, 2));
-}
-/**
  * is_divisible_by_i - Entry point
  *
  * @n: use sizeof
@@ -28,4 +15,17 @@ int is_divisible_by_i(int n, int i)
 	if (n % i == 0)
 		return (0);
 	return (is_divisible_by_i(n, i + 1));
+}
+/**
+ * is_prime_number - Entry point
+ *
+ * @n: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+int is_prime_number(int n)
+{
+	if (n == 1)
+		return (1);
+	return (is_divisible_by_i(n, 2));
 }
