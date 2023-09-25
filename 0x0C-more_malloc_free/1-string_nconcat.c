718681c8c8dd5c333d074e9abd47a1decf4a7322
peter <peteriskander75@gmail.com>
2023-09-25 04:57:53 -0700

    new

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int i = 0, j = 0;
 	char *conc;
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	conc = calloc(strlen(s1) + n + 1, sizeof(char));
 	if (conc == NULL)
