3d90e2363de892463bda5602e55429b843697685
peter <peteriskander75@gmail.com>
2023-09-26 08:55:02 -0700

    new

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - Entry point
+ *
+ * @head: use sizeof
+ *
+ * Return: 0 (Success)
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+	head = *head;
+
+	while (head)
+	{
+		ptr = head->next;
+		free(head);
+		head = ptr;
+	}
+	head = NULL;
+}
