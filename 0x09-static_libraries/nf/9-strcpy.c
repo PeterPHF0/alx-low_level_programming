64f23d822f63fed23785d849ffd491b42685bda3
PeterPHF0 <138907638+PeterPHF0@users.noreply.github.com>
2023-10-27 21:41:53 +0200

    Add files via upload

---
+++ 0x09-static_libraries/nf/9-strcpy.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * _strcpy - Entry point
+ *
+ * @dest: use sizeof
+ *
+ * @src: fasf
+ *
+ * Return: 0 (Success)
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int len = strlen(src);
+	int i = 0;
+
+	while (i <= len)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
