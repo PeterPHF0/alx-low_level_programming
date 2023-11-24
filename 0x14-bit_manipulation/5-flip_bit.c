decccc578091a4aedcf3ea3c817d01457e12f983
PeterPHF0 <138907638+PeterPHF0@users.noreply.github.com>
2023-11-25 01:08:28 +0200

    Update 5-flip_bit.c

--- 0x14-bit_manipulation/5-flip_bit.c
+++ 0x14-bit_manipulation/5-flip_bit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * flip_bits - counts the number of bits to change
- * to get from one number to another
+ * flip_bits -  change
+ * to get froto another
  * @n: first number
- * @m: second number
+ * @m: second 
  *
- * Return: number of bits to change
+ * Return: number hange
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
