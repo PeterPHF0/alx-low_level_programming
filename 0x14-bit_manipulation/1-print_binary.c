d2b7c4103d95660473b91dbcf1d95378c5f2a667
root <peteriskander75@gmail.com>
2023-11-24 14:46:30 -0800

    new

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <math.h>
+int power(int base, int exponent) {
+
+	if (exponent == 0) {
+		return (1);
+	} else {
+		return (base * power(base, exponent - 1));
+	}
+}
+
+
 /**
  * print_binary - check the code
  * @n: int number
@@ -8,7 +18,7 @@
 void print_binary(unsigned long int n)
 {
 	int exp = 0;
-	int two_pow_exp = (int)pow(2, exp);
+	unsigned long int two_pow_exp = (int)power(2, exp);
 	int i;
 
 	if (n == 0)
@@ -19,16 +29,16 @@ void print_binary(unsigned long int n)
 	while (two_pow_exp <= n)
 	{
 		exp++;
-		two_pow_exp = (int)pow(2, exp);
+		two_pow_exp = (int)power(2, exp);
 	}
 	exp = exp - 1;
-	two_pow_exp = (int)pow(2, exp);
+	two_pow_exp = (int)power(2, exp);
 	n = n - two_pow_exp;
 	printf("%d", 1);
 
 	for (i = exp - 1; i >= 0; i--)
 	{
-		two_pow_exp = (int)pow(2, i);
+		two_pow_exp = (int)power(2, i);
 		if (two_pow_exp <= n)
 		{
 			printf("%d", 1);
